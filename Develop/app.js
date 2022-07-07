6cb23caa4582d691edfc868eb8c150508c5ed82f
Ben Smerd <bsmerd@adam.com.au>
2022-07-07 17:02:14 +0930

    Created the algorithm for the time and if each row was past, present or future compared to current time in Javascript.

--- Develop/app.js
+++ Develop/app.js
@@ -1,49 +1,88 @@
 // variables
 var timerId = $('#currentDay');
 var containerId = $('.container')
+var rowContainer = $('<div>');
+var timeBlock = $('<div>');
+var textArea = $('<textarea>');
+var saveBtn = $('<button>');  
+
+
+var dateTime;
+var hour = moment().hour();
+var hourArray = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+var finalTime;
 
 // Setting the date with momentjs
-var dateTime = moment();
-timerId.text(dateTime.format("dddd, MMMM Do Y"))
+setInterval(function() {
+    dateTime = moment();
+    timerId.text(dateTime.format("dddd, MMMM Do Y HH:mm"))
+}, 1000);
 
+// function that creates the rows of schedules
+createRow = (timeString, timeInt) => {
+    rowContainer = $('<div>');
+    timeBlock = $('<div>');
+    textArea = $('<textarea>');
+    saveBtn = $('<button>');        
 
-// CREATE THE TIME BLOCKS FOR BUSINESS HOURS BETWEEN 9AM to 5PM COLOR CODED- GREY= EVENT HAPPENED, PINK= CURRENT HOUR, GREEN= UPCOMING. EACH TIMEBLOCK IS A TEXT AREA WHERE USER CAN ENTER TEXT USE TEXT AREA CLASS FROM CSS
-
-createRow = (time) => {
     // row
-    var rowContainer = $('<div>');
     rowContainer.addClass('row');
     rowContainer.css('display', 'flex');
     rowContainer.css('justify-content', 'space-between');
     containerId.append(rowContainer);
 
-
-
     // timeblock 
-    var timeBlock = $('<div>');
-    timeBlock.addClass('time-block');
-    timeBlock.text(time);
+    timeBlock.addClass('hour');
+    timeBlock.text(timeString);
     timeBlock.css('width', '15%');
     rowContainer.append(timeBlock);
 
-
-
     // textarea
-    var textArea = $('<textarea>');
     textArea.text("Hello there I have a meeting at this time"); //placeholder
     textArea.css('width', '70%');
     rowContainer.append(textArea);
 
-
-
     // save btn
-    var saveBtn = $('<button>');
     saveBtn.addClass('saveBtn');
     saveBtn.text('Save'); //placeholder put logo in
     saveBtn.css('width', '15%')
     rowContainer.append(saveBtn);
-};
 
+    var timeNew;
+    // if statement to convert the immutable moment.hour() function from 24 hour time to 12 hour time
+    if (hour >= 12) {
+        if (timeInt >= 1 && timeInt < 8) {  
+            timeNew = timeInt + 12
+            console.log(timeNew)
+        } else {
+            timeNew = timeInt;
+            console.log(timeNew)
+        };
+    } else {
+        if (timeInt >= 1 && timeInt < 8) {  
+            timeNew = timeInt + 12
+            console.log(timeNew)
+        } else {
+            timeNew = timeInt;
+        }
+    };
+    
+    // decide if the time is past, present or future compared to the schedule times
+    if (timeNew === hour) {
+        textArea.addClass('present');
+        console.log("equal" + timeNew)
+        console.log(hour)
+    } else if (timeNew < hour) {
+        textArea.addClass('past')
+        console.log("past" + timeNew)
+        console.log(hour)
+    } else {
+        textArea.addClass('future')
+        console.log("future" + timeNew)
+        console.log(hour)
+    }
+
+};
 
 
 
@@ -53,12 +92,31 @@ createRow = (time) => {
 // JQUERY AND THE SAVE BUTON IS PAIRED TO THE SIBLING OF THE INFO/TEXT BLOCK OF TIME
 
 
-createRow("9:00 am")
-createRow("10:00 am")
-createRow("11:00 am")
-createRow("12:00 pm")
-createRow("13:00 pm")
-createRow("14:00 pm")
-createRow("15:00 pm")
-createRow("16:00 pm")
-createRow("17:00 pm")
+// function that tajes in time and assigns it the past,present,future
+
+
+
+
+// This function takes in the hour and then assigns it the AM or PM period and calls the row creator function
+const amOrPm = function(hourInt) {
+    if(hourInt > 8 && hourInt < 12) {
+        finalTime = hourInt + "AM";
+        console.log(finalTime)
+        // pastPresentFuture(hourInt)
+        createRow(finalTime, hourInt);
+        
+    } else {
+        finalTime = hourInt + "PM";
+        console.log(finalTime)
+        // pastPresentFuture(hourInt)
+        createRow(finalTime, hourInt);
+    };
+};
+
+// This for loop iterates through the hour array and calls the amOrPm function
+for (var i = 0; i < hourArray.length; i++) {
+    
+    amOrPm(hourArray[i]);
+    
+};
+
